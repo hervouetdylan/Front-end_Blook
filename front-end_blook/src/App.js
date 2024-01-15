d1f0cbcae060e021e12519db3c54f61a75cf5f6e
JOUTIN <jules.outin@ynov.com>
2024-01-15 16:41:25 +0100

    Ajout de recettes sur la page d'accueil

--- front-end_blook/src/App.js
+++ front-end_blook/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card />
     </div>
   );
 }
