4a1cb548aca9f4e1ddf52e5f8f1641838891cc49
Jules OUTIN <90403617+JulesOutin@users.noreply.github.com>
2024-01-15 18:24:55 +0100

    ✨ Feat/navbar (#1)
    * Ajout de la navbar
    
    * Ajout de la navbar
    
    * Ajout du nom dans la navbar

--- front-end_blook/src/App.js
+++ front-end_blook/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
-      </header>
+      <Header />
     </div>
   );
 }
