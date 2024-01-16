79f0eb4852198e57a64979e25672d9377f77e6fe
JOUTIN <jules.outin@ynov.com>
2024-01-16 19:09:18 +0100

    :construction: Page recette en cours

--- front-end_blook/src/App.js
+++ front-end_blook/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
 import Card from "./components/Card";
+import Recette from "./components/Page_Recette"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (<>
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Card />} />
+        <Route path="/" element={<Card />} />
+        <Route path="/recette" element={<Recette />} />
       </Routes>
     </BrowserRouter></>
   );
--- front-end_blook/src/components/Page_Recette.js
+++ front-end_blook/src/components/Page_Recette.js
@@ -1,26 +1,26 @@
-
+import "./Page_Recette.css";
 
 
 function Recette() {
     return (
         <div className="display_recette">
             <h1>Recette</h1>
-            <img src="" alt="" />
+            <img className="img_recette" src="https://assets.afcdn.com/recipe/20190529/93153_w1024h576c1cx2220cy1728cxt0cyt0cxb4441cyb3456.jpg" alt="" />
             <h2>Temps de cuisson</h2>
             <p>1h</p>
             <h2>Nombre de portions</h2>
             <h2>Liste des ingrédients</h2>
-            <ol>
-                <li>1</li>
+            <ul className="">
+                <li className="">1</li>
                 <li>2</li>
                 <li>3</li>
-            </ol>
+            </ul>
             <h2>Étapes de préparation</h2>
-            <ol>
+            <ul>
                 <li>1</li>
                 <li>2</li>
                 <li>3</li>
-            </ol>
+            </ul>
         </div>
     )
 }
